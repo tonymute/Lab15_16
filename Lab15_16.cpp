2fa48c885ece7af1d9b176901994d77567540ded
Арсений Муравьёв <354asd@mail.ru>
2021-10-14 22:36:34 +0300

    Добавьте файлы проекта.

---
+++ Lab15_16.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, ".1251");
+
+	double k, ras, sto, sum;
+	cout << "Введите иходные данные" << endl << "Километраж: ";
+	cin >> k;
+	cout << "Расход топлива: ";
+	cin >> ras;
+	cout << "Стоимость топлива: ";
+	cin >> sto;
+	sum = k / 100 * ras * sto;
+	cout << "Стоимость поездки = " << sum << "\n\n\n";
+
+
+	system("PAUSE");
+}
